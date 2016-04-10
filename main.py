1a30dff585d9f197d7692075ee46c0f64615071b
Zhen@laptop <zhen.li.zl242@yale.edu>
2016-04-10 01:44:16 -0400

    Can display paired devices; Working on making connection

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import os
 import sys
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.gridlayout import GridLayout
 from math import sin
 from kivy.properties import ObjectProperty, NumericProperty, StringProperty, BooleanProperty, ListProperty
 from kivy.clock import Clock
@@ -18,18 +21,16 @@ import random
 import time
 from jnius import autoclass
 
+BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
 #BluetoothDevice = autoclass('android.bluetooth.BluetoothDevice')
 #BluetoothSocket = autoclass('android.bluetooth.BluetoothSocket')
-#UUID = autoclass('java.util.UUID')
+UUID = autoclass('java.util.UUID')
 # Create a listener function
 class ViViChart(Widget):
-    BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
     paired_device = StringProperty('No Paired Device')
     n_paired_devices = NumericProperty(0)
+    data = NumericProperty(0)
     
-    def paired(Button):
-        pass
-
     def __init__(self):
         super(ViViChart, self).__init__()
         graph_theme = {'background_color': 'f8f8f2'}
@@ -37,19 +38,15 @@ class ViViChart(Widget):
         self.plot = MeshLinePlot(color=[1, 0, 0, 1])
         self.plot.points = []
         self.graph.add_plot(self.plot)
-        self.start = time.time()               
+        self.start = time.time()
     
     def discover(self):
-        paired_devices = self.BluetoothAdapter.getDefaultAdapter().getBondedDevices().toArray()
-        #for device in paired_devices:
-        #    self.paired().add_widget()
-        #paired_devices = [random.random(), random.random()]         
-        #socket = None
+        paired_devices = BluetoothAdapter.getDefaultAdapter().getBondedDevices().toArray()
+        btns = []
         for device in paired_devices:
-            if device.getName() == 'MDR-XB950BT':
-                return device.getName()
-                break
-            '''
+            btns.append(Device(text = device.getName()))
+        return btns
+        '''
             if device.getName() == 'MDR-XB950BT':
                 socket = device.createRfcommSocketToServiceRecord(
                     self.UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
@@ -58,6 +55,16 @@ class ViViChart(Widget):
                 break
         socket.connect()
         '''
+    
+    def search_for_devices(self):
+        self.popup=Popup(title='Paired devices', size_hint = [0.8, 0.8],
+                         on_open=self.ids.popup2.dismiss)
+        btns = self.discover()
+        popup_grid = GridLayout(rows = len(btns))
+        for btn in btns:        
+            popup_grid.add_widget(btn) 
+        self.popup.add_widget(popup_grid)
+        self.popup.open()
         #return recv_stream
         #return len(paired_devices)#, recv_stream
     
@@ -75,17 +82,24 @@ class ViViChart(Widget):
         sys.stdout.write(".")
         sys.stdout.flush()
         #self.DataHandler(self, outfile)
-
-   # btn1 = Button(text="Bluetooth")
+class Device(Button):
+    def on_release(self):
+        paired_devices = BluetoothAdapter.getDefaultAdapter().getBondedDevices().toArray()
+        for device in paired_devices:
+            if device.getName() == self.text:
+                socket = device.createRfcommSocketToServiceRecord(
+                    UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+                break
+        recv_stream = socket.getInputStream()
+        return recv_stream
 
 class ViViTestApp(App):
     def build(self):
         # Clear and open the data file for writing
-        outfile = open("data.csv", "w")
+        outfile = open("vivipulse_data.csv", "w")
         # Write a header to the text file first thing
         outfile.write("Time, Data\n")
         chart = ViViChart()
-        chart.paired_device = chart.discover()
         Clock.schedule_interval(partial(chart.update, outfile), 1.0 / 60.0)
         return chart
      
