2e4d34eb5b4e8159b6bc7e6017ccbd5f52b4e83e
Zhen@laptop <zhen.li.zl242@yale.edu>
2016-04-10 23:29:21 -0400

    Update to version 0.2; Enabled receiving bluetooth data

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Apr  1 15:12:09 2016
-
+Updated on Sun Apr 10
+Version = 0.2
 @author: zhen
+
+Update note:
+Connecting to Bluetooth device and receive streams of data enabled
 """
 
 import os
@@ -11,6 +15,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from math import sin
 from kivy.properties import ObjectProperty, NumericProperty, StringProperty, BooleanProperty, ListProperty
@@ -22,14 +27,17 @@ import time
 from jnius import autoclass
 
 BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
-#BluetoothDevice = autoclass('android.bluetooth.BluetoothDevice')
-#BluetoothSocket = autoclass('android.bluetooth.BluetoothSocket')
+BluetoothDevice = autoclass('android.bluetooth.BluetoothDevice')
+BluetoothSocket = autoclass('android.bluetooth.BluetoothSocket')
+InputStreamReader = autoclass('java.io.InputStreamReader')
+BufferedReader = autoclass('java.io.BufferedReader')
 UUID = autoclass('java.util.UUID')
 # Create a listener function
 class ViViChart(Widget):
     paired_device = StringProperty('No Paired Device')
     n_paired_devices = NumericProperty(0)
-    data = NumericProperty(0)
+    data = StringProperty(0)
+    #exception = StringProperty(None)
     
     def __init__(self):
         super(ViViChart, self).__init__()
@@ -73,15 +81,26 @@ class ViViChart(Widget):
         self.graph.remove_plot(self.plot)
         self.plot.points = [( x, sin(x / 10.)) for x in range(0, int(200*random.random()) )] # This is just some mock data
         self.graph.add_plot(self.plot)
+        try:
+            recv_stream = BufferedReader(InputStreamReader(BluetoothSocket.getInputStream(), 'utf-8'))
+            #self.data = 1
+        except Exception:
+            #self.data = 2
+            pass
         #self.paired_device = self.get_socket_stream()
         #self.n_paired_devices = self.get_socket_stream()
         # Data logging
         now = time.time() - self.start # Generate a time stamp
-        data = ' ' #self.recv_stream
-        outfile.write(str(now) + "," + str(data)  + "\n")
-        sys.stdout.write(".")
-        sys.stdout.flush()
-        #self.DataHandler(self, outfile)
+        try:
+            self.data = recv_stream.readLine()
+            outfile.write(str(now) + "," + str(self.data)  + "\n")
+            sys.stdout.write(".")
+            sys.stdout.flush()
+            #self.DataHandler(self, outfile)
+        except Exception:
+            #self.data = 100
+            pass
+        
 class Device(Button):
     def on_release(self):
         paired_devices = BluetoothAdapter.getDefaultAdapter().getBondedDevices().toArray()
@@ -90,17 +109,22 @@ class Device(Button):
                 socket = device.createRfcommSocketToServiceRecord(
                     UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
                 break
-        recv_stream = socket.getInputStream()
-        return recv_stream
+        try:
+            socket.connect()
+        except Exception:
+            pass
+            
+class Exception_popup(Popup):
+    pass
 
 class ViViTestApp(App):
     def build(self):
         # Clear and open the data file for writing
-        outfile = open("vivipulse_data.csv", "w")
+        outfile = open(self.user_data_dir + "/vivipulse_data.csv", "w")
         # Write a header to the text file first thing
         outfile.write("Time, Data\n")
         chart = ViViChart()
-        Clock.schedule_interval(partial(chart.update, outfile), 1.0 / 60.0)
+        Clock.schedule_interval(partial(chart.update, outfile), 1.0/100)
         return chart
      
     def exit(self):
